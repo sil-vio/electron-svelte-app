b78a6bae47f17ab44447ebc848417e464c93f45a
Silvio Giannini <s.giannini@tdnet.it>
2019-12-29 22:48:36 +0100

    esempio funzionante modulo ytdl-core

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,62 @@
 <script>
-	import ytdl from 'ytdl-core'; 
-	import fs from 'fs';
+  import ytdl from "ytdl-core";
+  import fs from "fs";
+  import streamToBlob from "stream-to-blob";
+  import saveAs from "file-saver";
+  export let name;
+  let url;
 
-	export let name;
-	let url;
+  function handleClick() {
+    console.log(`URL: ${url}`);
+    const video = ytdl(url, {
+      quality: "highestaudio",
+      format: "mp3",
+      filter: "audioonly"
+    });
+    const blob = streamToBlob(video);
+        console.log("steram to blov");
 
-	function handleClick() {
-		console.log(`URL: ${url}`);
-		const video = ytdl(url, {
-			quality: "highestaudio",
-			format: "mp3",
-			filter: "audioonly"
-		});
-//		video.pipe(fs.createWriteStream("test.mp3"));
-	}
+    var myFile = blobToFile(blob, "audio.mp3");
+    console.log("steram to file: ", myFile);
+    myFile.then(value => {
+      console.log("feil ", value);
+      var filesaver = saveAs(value, "audio.mp3");
+    });
+
+  }
+
+  function blobToFile(theBlob, fileName) {
+    //A Blob() is almost a File() - it's just missing the two properties below which we will add
+    theBlob.lastModifiedDate = new Date();
+    theBlob.name = fileName;
+    return theBlob;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!!</h1>
-	<input bind:value={url} placeholder="enter video url">
-	<button on:click={handleClick}>Scarica</button>
-</main>
-
 <style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!!</h1>
+  <input bind:value={url} placeholder="enter video url" />
+  <button on:click={handleClick}>Scarica</button>
+</main>
